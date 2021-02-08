7c8dfd13fd981f36a48a53002120bbd7da9df34f
Azman M. Yusof <azman@my1matrix.org>
2021-02-08 15:09:43 +0800

    MOVED TO https://codeberg.org/azman/my1imgpro.git

--- src/my1video.c
+++
@@ -1,8 +0,0 @@
-/*----------------------------------------------------------------------------*/
-#ifndef __MY1IMAGE_DO_MAIN__
-#define __MY1IMAGE_DO_MAIN__
-#endif
-#include "my1image.c"
-#include "my1video_base.c"
-#include "my1video_data.c"
-/*----------------------------------------------------------------------------*/
